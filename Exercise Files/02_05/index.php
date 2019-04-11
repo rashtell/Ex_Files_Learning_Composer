eadc0fc907030238c54d339fe3dfdbbf4b9101ba
rashtell <rashtell2@gmail.com>
2019-04-11 20:05:13 +0100

    Ex_Files_Learning_Composer

---
+++ Exercise Files/02_05/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ImageAbstractor</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all">
+</head>
+
+<body>
+
+	<header class="masthead">
+		<div class="site-branding">
+			<h1 class="site-title">ImageAbstractor</h1>
+			<p class="site-description">Pull the most dominant colors from an image.</p>
+		</div><!-- .site-title -->
+	</header><!-- .masthead -->
+
+	<section class="the-grid">
+
+		<ul class="colors">
+
+			<?php
+
+				// Magical image abstraction things will happen here.
+
+			?>
+
+		</ul>
+
+	</section><!-- .the-grid -->
+
+</body>
+
+</html>
+
